1002bcb09baf6da5a59d533242e8d72d4bd8dac3
Leomar da Silva Barbosa <leomar.barbosa@kaiostech.com>
2019-08-15 12:37:17 -0400

    update

--- spec/Login_spec.rb
+++ spec/Login_spec.rb
@@ -8,13 +8,16 @@ describe 'Realizar Login', :login do
           @user = LoginPage.new
         end    
         
-        email_usuario = 'tony@stark.com'
-        senha_usuario = 'pwd123'
-        email_invalido = 'leomr@@@@@gmail.com'
-        senha_invalida = '1234567890'
-        senha_branco = ''
-        email_branco = ''
-        
+        # Base de dados para teste
+            email_usuario = 'tony@stark.com'
+            senha_usuario = 'pwd123'
+            email_invalido = 'leomr@@@@@gmail.com'
+            senha_invalida = '1234567890'
+            msg_email = 'Opps. Cadê o email?'
+            msg_senha = 'Opps. Cadê a senha?'
+            dados_invalidos = 'Usuário e/ou senha inválidos'
+            senha_branco = ''
+            email_branco = ''
 
         it 'com sucesso' do
             @user.insere_os_dados(email_usuario, senha_usuario)
@@ -27,7 +30,7 @@ describe 'Realizar Login', :login do
                 @user.insere_usuario_senha_em_brancos(email_branco, senha_branco)
                 @user.clica_no_botao_ENTRAR()
                 message = find('.alert span');
-                expect(message.text).to eql 'Opps. Cadê o email?'
+                expect(message.text).to eql msg_email
             rescue Error => e
                 puts e.message
             end    
@@ -38,7 +41,7 @@ describe 'Realizar Login', :login do
                 @user.insere_usuario_senha_em_brancos(email_usuario, senha_branco)
                 @user.clica_no_botao_ENTRAR()
                 message = find('.alert span');
-                expect(message.text).to eql 'Opps. Cadê a senha?'
+                expect(message.text).to eql msg_senha
             rescue Error => e
                 puts e.message
             end    
@@ -51,7 +54,7 @@ describe 'Realizar Login', :login do
                 @user.insere_email_invalido(email_invalido, senha_usuario)
                 @user.clica_no_botao_ENTRAR()
                 message = find('.alert span');
-                expect(message.text).to eql 'Usuário e/ou senha inválidos'
+                expect(message.text).to eql dados_invalidos
             rescue Error => e
                 puts e.message
             end     
